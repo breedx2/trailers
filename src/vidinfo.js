d2cd2e0257606feb5c2001e605af741305fd5220
jason <jason@noisybox.net>
2021-04-04 00:17:30 -0700

    add probe

--- src/vidinfo.js
+++ src/vidinfo.js
@@ -1,5 +1,5 @@
 'use strict';
-const { execFileSync } = require("child_process");
+const { execFileSync, spawnSync } = require("child_process");
 
 // Gets metadata about a video file and writes it to a descriptor
 
@@ -9,7 +9,7 @@ function cropdetect(file){
   const lines = cmdResult.split("\n");
   const result = {};
   lines.shift();
-  lines.forEach((line, i) => {
+  lines.forEach(line => {
     const [countStr, crop] = line.trim().split(/\s+/);
     const count = parseInt(countStr);
     // don't include super small intervals (< 10)
@@ -18,11 +18,34 @@ function cropdetect(file){
   return result;
 }
 
+function probe(file){
+    // const cmdResult = execFileSync("ffprobe", [file]).toString().trim();
+    const cmdResult = spawnSync("ffprobe", [file]);
+    const lines = cmdResult.stderr.toString().trim().split("\n");
+    lines.shift();
+    const result = {};
+    lines.forEach(line => {
+      if(line.trim().match(/^Duration:/)){
+        result.duration = line.trim().match(/^Duration: (\d\d:\d\d:\d\d.\d\d).*/)[1];
+      }
+      else if(line.trim().match(/^Stream #0:0: Video/)){
+        result.size = line.trim().match(/, (\d+x\d+) /)[1];
+        result.fps = line.trim().match(/ (\d+(\.\d+)?) fps/)[1];
+      }
+    });
+    return result;
+}
+
+
+
 if (require.main === module) {
   // Called directly
-  const cropInfo = cropdetect(process.argv[2]);
-  console.log(cropInfo);
+  // const cropInfo = cropdetect(process.argv[2]);
+  // console.log(cropInfo);
+  const probeInfo = probe(process.argv[2]);
+  console.log(probeInfo);
 }
+
 
 module.exports = {
   cropdetect
